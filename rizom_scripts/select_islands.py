98522ba0e3b83a56ba91e26a8b66b31b43f00813
Matt Ashpole <m.d.ashpole@gmail.com>
2025-02-27 02:16:45 +0000

    feat: new script for selecting islands by bounding box size.

---
+++ rizom_scripts/select_islands.py
@@ -0,0 +1,35 @@
+"""Island selection functions."""
+
+
+def select_islands_by_pixel_size(max_u: int, max_v: int):
+    """Select islands whose U or V pixel width is <= the given value.
+
+    Args:
+        max_u (int): Minimum U size to select.
+        max_v (int): Minimum V size to select.
+
+    """
+    current_uvmap: str = App.Get("Lib.CurrentUVSetName")
+    resolution: int = App.Get(f"Lib.UVSets.{current_uvmap}.RootGroup.Properties.Pack.MapResolution")
+    islands: dict = App.Get("Lib.Mesh.Islands")
+
+    islands_to_select = []
+    for id in islands.keys():
+        print(id)
+        u_minus, u_plus, v_minus, v_plus, *_ = App.Eval(f"Lib.Mesh.Islands.{id}.GetBBoxUVW")
+        u = (u_plus - u_minus) * resolution
+        v = (v_plus - v_minus) * resolution
+
+        if u <= max_u or v <= max_v:
+            islands_to_select.append(int(id))
+
+    App.Select(
+        {
+            "PrimType": "Island",
+            "WorkingSet": "Visible",
+            "ResetBefore": True,
+            "Select": True,
+            "IDs": islands_to_select,
+            "List": True,
+        }
+    )
